657ebdd4ac680956f3f0638424375039d88df56e
lsc <lishaochen_vip@163.com>
2020-04-16 13:08:08 +0800

    feat: vue通用骨架

--- app/vue.config.js
+++ app/vue.config.js
@@ -88,28 +88,28 @@ module.exports = {
       },
     },
   },
-  pages: {
-    index: {
-      // page 的入口
-      entry: 'src/main.js',
-      // 模板来源
-      template: 'public/index.html',
-      // 在 dist/index.html 的输出
-      filename: 'index.html',
-      // 当使用 title 选项时，
-      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'index',
-      // 提取出来的通用 chunk 和 vendor chunk。
-      chunks: ['chunk-vendors', 'chunk-common', 'index'],
-    },
-    login: {
-      entry: 'src/pages/login.js',
-      template: 'public/login.html',
-      filename: 'login.html',
-      title: 'login',
-      chunks: ['chunk-vendors', 'chunk-common', 'index'],
-    },
-  },
+  // pages: {
+  //   index: {
+  //     // page 的入口
+  //     entry: 'src/main.js',
+  //     // 模板来源
+  //     template: 'public/index.html',
+  //     // 在 dist/index.html 的输出
+  //     filename: 'index.html',
+  //     // 当使用 title 选项时，
+  //     // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+  //     title: 'index',
+  //     // 提取出来的通用 chunk 和 vendor chunk。
+  //     chunks: ['chunk-vendors', 'chunk-common', 'index'],
+  //   },
+  //   login: {
+  //     entry: 'src/pages/login.js',
+  //     template: 'public/login.html',
+  //     filename: 'login.html',
+  //     title: 'login',
+  //     chunks: ['chunk-vendors', 'chunk-common', 'index'],
+  //   },
+  // },
   chainWebpack: (config) => {
     // 修复HMR
     config.resolve.symlinks(true);
